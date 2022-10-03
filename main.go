8e82a365eb08154701ee42a116f91d1664276b5c
petteerimp <99396292+petteerimp@users.noreply.github.com>
2022-10-03 11:53:37 +0200

    My Second Updated
    Blah

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hej, världen!")
 	fmt.Println("Hello, this is my update 2022-10-03")
+	fmt.Println("Hello, this is my second update 2022-10-03 ... Hear my wrath!")
 }
 
 func add(x, y float) float {
